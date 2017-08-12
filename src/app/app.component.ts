f35134d710b77e2405da61d20bda290bddfad095
Moraru Viorel <VMoraru@ASP>
2017-08-13 00:38:53 +0300

    show point list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My first AGM project';
+  lat = 51.678418;
+  lng = 7.809007;
+  zoom = 8;
+
+  markers: marker[] = [
+  {
+    lat: 51.673858,
+    lng: 7.815982,
+    label: 'A',
+    draggable: true
+  },
+  {
+    lat: 51.373858,
+    lng: 7.215982,
+    label: 'B',
+    draggable: false
+  },
+  {
+    lat: 51.723858,
+    lng: 7.895982,
+    label: 'C',
+    draggable: true
+  // tslint:disable-next-line:semicolon
+  }]
+}
+
+// tslint:disable-next-line:class-name
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
 }
